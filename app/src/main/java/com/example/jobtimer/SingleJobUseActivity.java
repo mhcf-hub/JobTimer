14b5f443fe17cf4aa0fcd616b6f69c03d0ced0e3
KaGe <info@max-hoffmann-webdesign.de>
2020-03-06 11:37:30 +0100

    Hello test

--- app/src/main/java/com/example/jobtimer/SingleJobUseActivity.java
+++ app/src/main/java/com/example/jobtimer/SingleJobUseActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class SingleJobUseActivity extends AppCompatActivity {
 
+    int test;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
