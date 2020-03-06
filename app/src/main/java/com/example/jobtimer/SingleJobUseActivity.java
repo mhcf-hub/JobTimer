97e24245c1ff981de869c7cc835cd5193efb4d17
KaGe <info@max-hoffmann-webdesign.de>
2020-03-06 11:35:48 +0100

    Hello test

---
+++ app/src/main/java/com/example/jobtimer/SingleJobUseActivity.java
@@ -0,0 +1,14 @@
+package com.example.jobtimer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SingleJobUseActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_job_use);
+    }
+}
