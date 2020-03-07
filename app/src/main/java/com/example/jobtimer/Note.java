f7eb25dcd3674633ad70d71eb663a01c1f23b356
KaGe <info@max-hoffmann-webdesign.de>
2020-03-07 20:16:59 +0100

    Hello test

---
+++ app/src/main/java/com/example/jobtimer/Note.java
@@ -0,0 +1,59 @@
+package com.example.jobtimer;
+
+import androidx.annotation.NonNull;
+import androidx.room.ColumnInfo;
+import androidx.room.Entity;
+import androidx.room.ForeignKey;
+import androidx.room.Index;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "timings_table",
+        foreignKeys = @ForeignKey(entity = RoomJob.class,
+                parentColumns = "rid",
+                childColumns = "jobId",
+                onDelete = ForeignKey.CASCADE),
+        indices = {@Index("jobId"), @Index("content")})
+public class Note {
+
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    private int id;
+
+
+    @NonNull
+    @ColumnInfo(name = "jobId")
+    private int jobId;
+
+    @NonNull
+    @ColumnInfo(name = "date")
+    String content;
+
+    public Note(@NonNull String content) {
+        this.content = content;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getJobId() {
+        return jobId;
+    }
+
+    public void setJobId(int jobId) {
+        this.jobId = jobId;
+    }
+
+    @NonNull
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(@NonNull String content) {
+        this.content = content;
+    }
+}
